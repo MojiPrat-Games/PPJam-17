43d4150758bb0ce00e39d05b0978d410e3b4fceb
Maujey <mojizgamedev@gmail.com>
2025-03-01 14:31:40 +0530

    Revert "Added pickup system"
    This reverts commit 85dfbfac8525c329025566b5ea8dbbda789929c6, reversing
    changes made to 7f4f2bfe3434bf9d19738d16c4d2307a8a6e5b6e.

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class FirstPersonController : MonoBehaviour
 {
     private CharacterController controller;
     private Camera playerCamera;
